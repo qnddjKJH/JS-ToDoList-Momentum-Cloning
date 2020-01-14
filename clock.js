bf3239211be383ad4e0b400bc2df3e5856468395
qnddjKJH <qnddjKJH.dev@gmail.com>
2020-01-14 16:58:34 +0900

    3.1강 - 3.5강
     * 시계 구현
     * Local Storage
     * Input - Save & Load
     * ToDoList Print

---
+++ clock.js
@@ -0,0 +1,26 @@
+// 변수의 값이 변하지 않는 한 Javascript 에서는 const 상수로 선언한다.
+
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = 
+        (hours < 10 ? '0'+hours : hours) + ':' + 
+        (minutes < 10 ? '0' + minutes : minutes) + ':' + 
+        (seconds < 10 ? '0' + seconds : seconds);
+
+    // 리터럴 문법
+    // clockTitle.innerText = 
+    //  '${hours < 10 ? '0${hours}' : hours}:${minutes < 10 ? '0${minutes}' : minutes}:${seconds < 10 ? '0${seconds}' : seconds}';
+}
+
+function init () {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
